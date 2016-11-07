9e4d2b184de277ee67d56cf81435ab695609e48e
Ian Firkin <ian.firkin@gisinc.com>
2016-11-07 09:37:02 -0500

    Adds SFC template

---
+++ React SFC.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ${NAME} = (props) => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+${NAME}.displayName = '${NAME}';
+
+${NAME}.propTypes = {
+  
+};
+
+export default ${NAME};
